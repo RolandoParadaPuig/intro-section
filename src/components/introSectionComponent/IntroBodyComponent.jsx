a2483eafe73b9c1f1a6f2f222570e7763b2bdd0b
Rolando Parada <rolandoparadapuig@gmail.com>
2022-06-11 11:41:50 -0400

    finish

--- src/components/introSectionComponent/IntroBodyComponent.jsx
+++ src/components/introSectionComponent/IntroBodyComponent.jsx
@@ -7,29 +7,35 @@ export const IntroBodyComponent = () => {
       <div className={"body--image-container"}>
         <img
           src="src\images\mobile\image-hero-mobile.png"
-          className="body--image"
+          className="body--image-mobile"
+        />
+        <img
+          src="src\images\desktop\image-hero-desktop.png"
+          className="body--image-desktop"
         />
       </div>
-      <div className="body--text-container">
-        <h1 className="body--title">Make remote work</h1>
-        <p className="body--text-content">
-          Get your team in sync, no matter your location. Streampline processes,
-          create team rituals, and watch productivity soar
-        </p>
-        <button
-          onClick={() => {
-            message.success("you clicked!");
-          }}
-          className="body--button"
-        >
-          Learn more
-        </button>
-      </div>
-      <div className="body--client-container">
-        <div className="body--client databiz" />
-        <div className="body--client audiophile" />
-        <div className="body--client meet" />
-        <div className="body--client maker" />
+      <div className="body--main-container">
+        <div className="body--text-container">
+          <h1 className="body--title">Make remote work</h1>
+          <p className="body--text-content">
+            Get your team in sync, no matter your location. Streampline
+            processes, create team rituals, and watch productivity soar
+          </p>
+          <button
+            onClick={() => {
+              message.success("you clicked!");
+            }}
+            className="body--button"
+          >
+            Learn more
+          </button>
+        </div>
+        <div className="body--client-container">
+          <div className="body--client databiz" />
+          <div className="body--client audiophile" />
+          <div className="body--client meet" />
+          <div className="body--client maker" />
+        </div>
       </div>
     </div>
   );
--- src/components/introSectionComponent/IntroHeaderComponent.jsx
+++ src/components/introSectionComponent/IntroHeaderComponent.jsx
@@ -1,72 +1,136 @@
-import { Button, Col, Dropdown, Row } from "antd";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Button, Col, Dropdown, Menu, Row } from "antd";
 import "./introHeaderComponent.css";
 import { CloseOutlined } from "@ant-design/icons";
 import React, { useState } from "react";
+
 export const IntroHeaderComponent = () => {
-  const [toggleArrowF, setToggleArrowF] = useState(false);
-  const [toggleArrowC, setToggleArrowC] = useState(false);
-  const onEnterF = () => {
-    setToggleArrowF(!toggleArrowF);
-  };
-  const onEnterC = () => {
-    setToggleArrowC(!toggleArrowC);
-  };
-  const btnClick = (e) => {
+  const [menuDisplayed, setMenuDisplayed] = useState(false);
+  const featuresClick = (e) => {
+    const featureListElem = document.querySelector("#featureList");
+    const classListChildren = e.target.children[0].classList;
     const classList = e.target.classList;
+    featureListElem.classList.toggle("header--navigation-display-features");
+    classList.toggle("header--navigation-btn-selected");
+    classListChildren.toggle("header--iconDown");
+    classListChildren.toggle("header--iconUp");
+  };
+  const companyClick = (e) => {
+    const featureListElem = document.querySelector("#companyList");
+    const classListChildren = e.target.children[0].classList;
+    const classList = e.target.classList;
+    featureListElem.classList.toggle("header--navigation-display-company");
+    classList.toggle("header--navigation-btn-selected");
+    classListChildren.toggle("header--iconDown");
+    classListChildren.toggle("header--iconUp");
+  };
+  const btnMinClick = (e) => {
+    setMenuDisplayed(!menuDisplayed);
+    const classList = e.target.classList;
+    const displayMenu = document.querySelector("#menuContainer");
+    !menuDisplayed
+      ? (displayMenu.style.width = "100vw")
+      : (displayMenu.style.width = "0");
     classList.toggle("header--btn-line-1");
     classList.toggle("header--btn-line-2");
-    console.log(classList);
   };
+
   return (
     <header className={"header--component"}>
       <Row justify="space-between">
-        <Col align="center" xs={4}>
+        <Col align="center" xs={2} lg={2}>
           <div className={"header--logo"} />
         </Col>
-        <Col align="center" xs={0}>
+        <div id="menuContainer" className={"header--navigation-menu-container"}>
           <nav className={"header--navigation-menu"}>
-            <button
-              type="link"
-              onMouseEnter={onEnterF}
-              onMouseLeave={onEnterF}
-              className={"header--navigation-btn"}
-            >
-              Features <div className="header--iconDown" />
-              <div className="header--iconUp" />
-            </button>
-            <button
-              type="link"
-              onMouseEnter={onEnterC}
-              onMouseLeave={onEnterC}
-              className={"header--navigation-btn"}
-            >
-              Company{" "}
-              {!toggleArrowC ? (
-                <div className="header--iconDown" />
-              ) : (
-                <div className="header--iconUp" />
-              )}
-            </button>
-            <button type="link" className={"header--navigation-btn"}>
-              Careers
-            </button>
-            <button type="link" className={"header--navigation-btn"}>
-              About
-            </button>
+            <ul className={"header--navigation-list"}>
+              <li>
+                <button
+                  onClick={featuresClick}
+                  className={"header--navigation-btn"}
+                >
+                  Features <div className="header--iconDown" />
+                </button>
+                <ul
+                  id="featureList"
+                  className={"header--navigation-features-list"}
+                >
+                  <li>
+                    <button className="header--navigation-features-btn ">
+                      <div className="todo-btn" /> Todo List
+                    </button>
+                  </li>
+                  <li>
+                    <button className="header--navigation-features-btn ">
+                      <div className="calendar-btn" /> Calendar
+                    </button>
+                  </li>
+                  <li>
+                    <button className="header--navigation-features-btn ">
+                      <div className="reminders-btn" /> Reminders
+                    </button>
+                  </li>
+                  <li>
+                    <button className="header--navigation-features-btn ">
+                      <div className="planning-btn" /> Planning
+                    </button>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <button
+                  onClick={companyClick}
+                  className={"header--navigation-btn"}
+                >
+                  Company <div className="header--iconDown" />
+                </button>
+                <ul
+                  id="companyList"
+                  className={"header--navigation-company-list"}
+                >
+                  <li>
+                    <button className="header--navigation-company-btn">
+                      History
+                    </button>
+                  </li>
+                  <li>
+                    <button className="header--navigation-company-btn">
+                      Our Team
+                    </button>
+                  </li>
+                  <li>
+                    <button className="header--navigation-company-btn">
+                      Blog
+                    </button>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <button className={"header--navigation-btn"}>Careers</button>
+              </li>
+              <li>
+                <button className={"header--navigation-btn"}>About</button>
+              </li>
+            </ul>
+            <ul className={"header--navigation-auth"}>
+              <li className="header--navigation-login">
+                <button className={"header--navigation-btn login-btn"}>
+                  Login
+                </button>
+              </li>
+              <button className={"header--navigation-btn register-btn"}>
+                Register
+              </button>
+              <li className="header--navigation-register"></li>
+            </ul>
           </nav>
-        </Col>
-        <Col align="center" xs={0}>
-          <div className={"header--navigation-auth"}>
-            <button type="link" className={"header--navigation-btn"}>
-              Login
-            </button>
-            <button className={"header--navigation-btn register"}>
-              Register
-            </button>
-          </div>
-        </Col>
-        <Col xs={2}>
-          <button onClick={btnClick} className="header--btn-mobile">
+        </div>
+        <Col xs={{ span: 2, offset: 20 }}>
+          <button onClick={btnMinClick} className="header--btn-mobile">
             <div className={"header--btn-line-1"} />
           </button>
         </Col>
